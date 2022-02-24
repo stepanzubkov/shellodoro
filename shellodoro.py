aeffd93daca1e710bc310ae47e76dce90cc2929a
Степан Зубков <83695097+Stepan-Zubkov@users.noreply.github.com>
2022-02-24 09:51:09 +0300

    Make stats and modes unique to each person

--- shellodoro.py
+++ shellodoro.py
@@ -2,8 +2,76 @@ import click
 import sys
 import time
 import json
+from pathlib import Path
 
 from tools import ftime, send_notify, add_pomodoro, get_json, to_graph
+
+
+if not Path('modes.json').is_file():
+    with open('modes.json', 'w') as f:
+        f.write(
+            '''
+{
+    "default": {
+        "work_time": 20,
+        "break_time": 5,
+        "long_break_time": 15,
+        "long_break_freq": 4
+    },
+    "52/17": {
+        "work_time": 52,
+        "break_time": 17,
+        "long_break_time": 30,
+        "long_break_freq": 4
+    },
+    "90/30": {
+        "work_time": 90,
+        "break_time": 30,
+        "long_break_time": 45,
+        "long_break_freq": 4
+    }
+}
+            '''
+        )
+
+if not Path('stats.json').is_file():
+    with open('stats.json', 'w') as f:
+        f.write(
+            '''
+{
+    "days": [
+        {
+            "name": "Monday",
+            "pomodoros": 0
+        },
+        {
+            "name": "Tuesday",
+            "pomodoros": 0
+        },
+        {
+            "name": "Wednesday",
+            "pomodoros": 0
+        },
+        {
+            "name": "Thursday",
+            "pomodoros": 0
+        },
+        {
+            "name": "Friday",
+            "pomodoros": 0
+        },
+        {
+            "name": "Saturday",
+            "pomodoros": 0
+        },
+        {
+            "name": "Sunday",
+            "pomodoros": 0
+        }
+    ]
+}
+            '''
+        )
 
 
 @click.group(invoke_without_command=True)

eba5eb129122f5bdef6a4c413bd6a239c03723c8
Степан Зубков <zubkovbackend@gmail.com>
2022-09-25 17:07:20 +0300

    1.0.5: Add star button to README

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="UTF-8") as f:
 
 setuptools.setup(
     name="shellodoro",
-    version="1.0.4",
+    version="1.0.5",
     author="StepanZubkov",
     author_email="zubkovbackend@gmail.com",
     description="Pomodoro timer right in your favorite terminal!",
